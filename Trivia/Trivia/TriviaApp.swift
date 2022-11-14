c6ac63923a1672e09287e8d529e1ad5d6ac10ebc
Stefan Bayne <stefanbayne@gmail.com>
2022-11-14 07:54:28 -0500

    FIrst commit.

---
+++ Trivia/Trivia/TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaApp.swift
+//  Trivia
+//
+//  Created by Stefan Bayne on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
